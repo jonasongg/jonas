277d008d8977488c98ce9b33f1d684fd654f89dc
jonasongg <jonasong2@gmail.com>
2024-03-26 00:20:13 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const cvwo = document.querySelector(".cvwo>a>div>img");
+
+const startGif = () => {
+  cvwo.src = "https://www.comp.nus.edu.sg/~vwo/assets/img/projects/2023-lbsa/singpass_2fa.gif";
+};
+
+const stopGif = () => {
+  cvwo.src = "https://www.comp.nus.edu.sg/~vwo/assets/img/projects/2023-lbsa/review_monthly_statements_react.png";
+};
+
+const scrollContainer = document.getElementById("parallax");
+
+const setScrollVar = () => {
+  const htmlElement = document.documentElement;
+  htmlElement.style.setProperty("--scroll", Math.min(1, scrollContainer.scrollTop / htmlElement.clientHeight));
+}
+
+scrollContainer.addEventListener("scroll", setScrollVar);
+scrollContainer.addEventListener("resize", setScrollVar);
+
+setScrollVar();
+
+const toggleExpand = (e) => {
+  // console.log(e.childNodes)
+  const description = e.childNodes[5];
+  description.classList.add("expand");
+}
